b62a3c4baa38d0623e5ad9c3fdc62f42ecdaeabd
Connor <connor.podesta.2021@uni.strath.ac.uk>
2023-01-31 18:34:48 +0000

    Refactored Parsing into seperate function on new file:    File will hold all parsing functions

--- main.c
+++ main.c
@@ -2,35 +2,14 @@
 #include <stdbool.h>
 #include <string.h>
 
+#include "parsing.h"
+
 #define MAX_INPUT 512
 
 int main(void) {
     bool running = true;
-    char input[MAX_INPUT];
 
     while (running) {
-        bool parsingLine = true;
-
-        printf("simpsh> ");
-
-        char* inputPtr = fgets(input, MAX_INPUT, stdin);
-        if (inputPtr == NULL) {
-            running = parsingLine = false;
-            printf("\n");
-        }
-
-        char* token = strtok(input, " \t\n|><&;");
-        if (token == NULL)
-            parsingLine = false;
-        
-        
-        while (parsingLine) {
-            printf("%s\n", token);
-            if (strcmp(token, "exit") == 0) 
-                running = parsingLine = false;
-            token = strtok(NULL, " \t\n|><&;");
-            if (token == NULL)
-                parsingLine = false;
-        }
+        running = parse();
     }
 }
---
+++ parsing.c
@@ -0,0 +1,34 @@
+//
+// Created by connor on 31/01/23.
+//
+
+#include "parsing.h"
+
+bool parse() {
+    char input[MAX_INPUT];
+    bool parsingLine = true;
+
+    printf("simpsh> ");
+
+    char* inputPtr = fgets(input, MAX_INPUT, stdin);
+    if (inputPtr == NULL) {
+        printf("\n");
+        parsingLine = false;
+        return false;
+    }
+
+    char* token = strtok(input, " \t\n|><&;");
+    if (token == NULL)
+        parsingLine = false;
+
+
+    while (parsingLine) {
+        printf("%s\n", token);
+        if (strcmp(token, "exit") == 0)
+            return parsingLine = false;
+        token = strtok(NULL, " \t\n|><&;");
+        if (token == NULL)
+            parsingLine = false;
+    }
+    return true;
+}
