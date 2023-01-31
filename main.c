d92d27a95ce7bc946f38ae2e0b9f580148f7f273
Cian Toner <cian.toner.2021@uni.strath.ac.uk>
2023-01-31 11:53:49 +0000

    implemented input string parsing,  exit conditions

--- main.c
+++ main.c
@@ -1,10 +1,32 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
 
 int main(void) {
     bool running = true;
+    char input[512];
 
     while (running) {
+        bool parsingLine = true;
+
         printf("simpsh> ");
+
+        // TODO: Rename penis before comitting
+        char* penis = fgets(input, 512, stdin);
+        if (penis == NULL) {
+            running = parsingLine = false;
+            printf("\n");
+        }
+
+        char* token = strtok(input, " \t\n|><&;");
+        
+        while (parsingLine) {
+            printf("%s\n", token);
+            if (strcmp(token, "exit") == 0) 
+                running = parsingLine = false;
+            token = strtok(NULL, " \t\n|><&;");
+            if (token == NULL)
+                parsingLine = false;
+        }
     }
 }
