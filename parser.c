cc598a05d5ed1c24ffd3f73cd357c32b6c39d700
Cian Toner <cian.toner.2021@uni.strath.ac.uk>
2023-02-04 14:47:41 +0000

    Consistent grammar

--- parser.c
+++ parser.c
@@ -9,30 +9,30 @@ bool parse() {
     // Define input buffer
     char input[MAX_INPUT];
 
-    // print user prompt
+    // Print user prompt
     printf("simpsh 8---->");
 
-    // get user input and store it into a character pointer that then pushes into the input buffer declared on line 8
+    // Get user input and store it into a character pointer that then pushes into the input buffer declared on line 8
     char* inputPtr = fgets(input, MAX_INPUT, stdin);
 
-    // if input starts with a NULL value, quit terminal
+    // If input starts with a NULL value, quit terminal
     if (inputPtr == NULL) {
         printf("\n");
         return false;
     }
 
-    // take first keyword off of input line, keywords are defined by words before the parsing characters (2nd input to fgets)
+    // Take first keyword off of input line, keywords are defined by words before the parsing characters (2nd input to fgets)
     char* token = strtok(input, DELIMITERS);
 
-    // if token is null value ignore parsing
+    // If token is null value ignore parsing
     while (token != NULL) {
         printf("%s\n", token);
 
-        // if the word is the exit command, terminate shell
+        // If the token is the exit command, terminate shell
         if (strcmp(token, "exit") == 0)
             return false;
 
-        // get next keyword
+        // Otherwise get next keyword and repeat
         token = strtok(NULL, DELIMITERS);
 
     }
