0a891e3b5345eef2606b2216a9c878746063aa82
xbb21163 <robbie.booth.2021@uni.strath.ac.uk>
2023-02-09 08:37:24 +0000

    Part_2 included history parts for part 5 which shouldn't be there so they have been removed

--- main.c
+++ main.c
@@ -1,15 +1,12 @@
 #include "parser.h"
-#include "history.h"
 #include "ExecuteCommand.h"
 
 int main() {
     char input[MAX_INPUT];
     char** tokens = parse(input);
-    char*  history[HISTORY_SIZE];
 
     while (tokens != NULL) {
         execute(tokens);
         tokens = parse(input);
-        checkHist(tokens, history);
     }
 }
