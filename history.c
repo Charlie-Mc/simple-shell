b9736db3f2eb0b7cb986f454a546ef3906d11952
Connor <connor.podesta.2021@uni.strath.ac.uk>
2023-02-13 21:01:16 +0000

    Working history however memory leak somewhere

--- history.c
+++ history.c
@@ -28,51 +28,35 @@ void push(char** des, char** value) {
     }
 }
 
-void revArr(char** src) {
-    char* temp = *src;
-    int i = 0;
 
-    while (strcmp(*src, "") != 0) {
-        src++;
-        i++;
-    }
-
-    char** des = malloc(sizeof(char*) * i);
-    for (int j = 0; j < i; j++) {
-        *des = *src;
-        des++;
-        src--;
-    }
-    src = des;
-    free(des);
-}
-
-int checkHist(char** tokens, char** history) {
+int checkHist(bool prevCalled, char** tokens, char** history) {
     if (strcmp(tokens[0], "!!") == 0) {
         if (strcmp(history[0], "") == 0) {
             printf("\nNo Command History Found!\n");
-            return 1;
+            return 2;
         } else {
             tokens[0] = *history;
         }
-        return 1;
+        return 0;
     } else if (strcmp(tokens[0], "history") == 0) {
         printHistory(history);
-        push(history, tokens);
-        return 1;
+        if (!prevCalled)
+            push(history, tokens);
+        return 2;
     } else if (strcmp(tokens[0], "ls") == 0) {
-        push(history, tokens);
-        printf("ls Tried");
+        if (!prevCalled)
+            push(history, tokens);
+        printf("ls Tried\n");
+        return 2;
     }
     push(history, tokens);
-    return 0;
+    return 1;
 }
 
 void printHistory(char** history) {
     if (*history == NULL) {
         *history = malloc(sizeof(char*));
     }
-    char* latestCall = *history;
     int i = 0;
     if (strcmp(*history, "") == 0) {
         printf("No Command History Found!\n");
