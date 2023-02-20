2dbaf7fef039eb2ad1b62e5ec6650ae673c30418
Connor <connor.podesta.2021@uni.strath.ac.uk>
2023-02-20 22:09:30 +0000

    !n now works without fault (i think) test vigourosly)
    NOTE: CURRENTLY WILL NOT WORK WITH MULTI TOKEN COMMANDS, toStr() FUNCTION NEEDS TWEEKED

--- history.c
+++ history.c
@@ -22,7 +22,32 @@ int checkHist(bool prevCalled, char** tokens, List history) {
             return 2;
         } else {
             free(tokens);
-            tokens = parse(get_at(history, size(history)-1));
+            tokens = parse(get_at(history, 0));
+        }
+        return 0;
+    } else if (*tokens[0] == '!'){
+        // get input value from input token
+        int val = tokens[0][1] - 48;
+        if (tokens[0][2] != '\0')
+            val = (val * 10) + (tokens[0][2] - 48);
+        if (tokens[0][3] != '\0') {
+            printf("\nLocation Invalid!\n");
+            return 2;
+        }
+        if (val < 1 || val > 20) {
+            printf("\nLocation Invalid!\n");
+            return 2;
+        }
+
+        // check for valid commands at location given
+        if (size(history) < val) {
+            printf("\nNo Command History Found At Location Given\n");
+            return 2;
+        }
+        // parse command at location given
+        else {
+            free(tokens);
+            tokens = parse(get_at(history, val-1));
         }
         return 0;
     } else if (strcmp(tokens[0], "history") == 0) {
