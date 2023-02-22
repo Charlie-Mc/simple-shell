12df61752a4a2b2414ccdb9386be67b9fb1ee24f
Cian Toner <cian.toner.2021@uni.strath.ac.uk>
2023-02-22 10:07:25 +0000

    Remove newlines from beginning of strings

--- history.c
+++ history.c
@@ -31,11 +31,11 @@ int checkHist(bool prevCalled, char*** tokensPtr, List history) {
     char** tokens = *tokensPtr;
     if (strcmp(tokens[0], "!!") == 0) {
         if (peek(history) == NULL) {
-            printf("\nNo Command History Found!\n");
+            printf("No Command History Found!\n");
             return 2;
         }
         if (strcmp(peek(history), "") == 0) {
-            printf("\nNo Command History Found!\n");
+            printf("No Command History Found!\n");
             return 2;
         } else {
             free(*tokensPtr);
@@ -48,17 +48,17 @@ int checkHist(bool prevCalled, char*** tokensPtr, List history) {
         if (tokens[0][2] != '\0')
             val = (val * 10) + (tokens[0][2] - 48);
         if (tokens[0][3] != '\0') {
-            printf("\nLocation Invalid!\n");
+            printf("Location Invalid!\n");
             return 2;
         }
         if (val < 1 || val > 20) {
-            printf("\nLocation Invalid!\n");
+            printf("Location Invalid!\n");
             return 2;
         }
 
         // check for valid commands at location given
         if (size(history) < val) {
-            printf("\nNo Command History Found At Location Given\n");
+            printf("No Command History Found At Location Given\n");
             return 2;
         }
         // parse command at location given
